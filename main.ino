4d1fcbec072a37d48c93c1f16dac5d4d273c5baa
Ahmed Abdeldaim <a7medabdeldaim@gmail.com>
2020-11-03 08:46:54 +0200

    Update main.ino

--- main.ino
+++ main.ino
@@ -95,7 +95,7 @@ char jam_char[] = {
   'n',
   'n'
 };
-int jam_a, jam_b, jam_c, jam_d, max_jam_val, current_route_val;
+int jam_a, jam_b, jam_c, jam_d;
 char max_jam, current_route;
 static int counter = 99;
 int jam_ind = 0;
@@ -123,38 +123,29 @@ void loop() {
   display();
 }
 void get_jam() {
-  /*
-  ** if route in jam_dict and value = current value then escape
-  ** if route not in jam_dict the add it and add value and add time
-  ** if route in jam_dict and value != current value then update current value and time
-  ** get index of the route
-  */
-  
-  /*
-  ** Route A
-  */
+  // if route in jam_dict and value = current value then escape
+  // if route not in jam_dict the add it and add value and add time
+  // if route in jam_dict and value != current value then update current value and time
+  // get index of the route
   int ind_a = find('a');
-  if (ind_a == -1 && jam_a != 0) {        // Route Not Found in Jam_Char Array
-    int ind_null = find('n');             // Get the first empty index in the array
-    if (ind_null != -1) {                 // Add it
+  if (ind_a == -1 && jam_a != 0) { // Route Not Found in Jam_Char Array
+    int ind_null = find('n'); // Get the first empty index in the array
+    if (ind_null != -1) { // Add it
       jam_char[ind_null] = 'a';
       jam_val[ind_null] = jam_a;
       jam_time[ind_null] = (int) time(NULL);
     }
-  } else if (jam_a == 0 && ind_a != -1) {   // Route Found but not Jammed
+  } else if (jam_a == 0 && ind_a != -1) { // Route Found but not Jammed
     jam_char[ind_a] = 'n';
     jam_val[ind_a] = 0;
     jam_time[ind_a] = 0;
-  } else if (ind_a != -1 && jam_val[ind_a] != jam_a) {  // Route Found in Jam_char array but current value is different
+  } else if (ind_a != -1 && jam_val[ind_a] != jam_a) { // Route Found in Jam_char array but current value is different
     jam_val[ind_a] = jam_a;
     jam_time[ind_a] = (int) time(NULL);
   }
-  
-  /*
-  ** Route B
-  */
-  
-  int ind_b = find('b'); // get index of the route
+  //////////////////////////////////////////////////////////////////////
+  // get index of the route
+  int ind_b = find('b');
   if (ind_b == -1 && jam_b != 0) { // Route Not Found in Jam_Char Array
     int ind_null = find('n'); // Get the first empty index in the array
     if (ind_null != -1) { // Add it
@@ -170,11 +161,6 @@ void get_jam() {
     jam_val[ind_b] = jam_b;
     jam_time[ind_b] = (int) time(NULL);
   }
-  
-  /*
-  ** Route C
-  */
-  
   // get index of the route
   int ind_c = find('c');
   if (ind_c == -1 && jam_c != 0) { // Route Not Found in Jam_Char Array
@@ -192,11 +178,6 @@ void get_jam() {
     jam_val[ind_c] = jam_c;
     jam_time[ind_c] = (int) time(NULL);
   }
-  
-  /*
-  ** Route D
-  */
-  
   // get index of the route
   int ind_d = find('d');
   if (ind_d == -1 && jam_d != 0) { // Route Not Found in Jam_Char Array
@@ -216,12 +197,9 @@ void get_jam() {
   }
 }
 void sort() {
-  /*
-  ** sort by the maximum value
-  ** then sort by time if only equal values
-  ** then add current route to the begining
-  */
-  
+  // sort by the maximum value
+  // then sort by time if only equal values
+  // then add current route to the begining
   int i, j, tmp;
   char tmp_char;
   for (i = 0; i < SIZE; i++) {
@@ -233,80 +211,60 @@ void sort() {
         tmp_char = jam_char[i];
         jam_char[i] = jam_char[j];
         jam_char[j] = tmp_char;
-        tmp = jam_time[i];
-        jam_time[i] = jam_time[j];
-        jam_time[j] = tmp;
+        tmp = jam_val[i];
+        jam_val[i] = jam_val[j];
+        jam_val[j] = tmp;
       }
     }
   }
   max_jam = jam_char[1];
-  max_jam_val = jam_val[1];
-  
-  // TODO: Check this Bug
   // Add the current route to the begining of the array
-  //  int ind = find(current_route);
-  //  if (ind != -1 && ind != 0) {
-  //    int temp_val = jam_val[ind];
-  //    int temp_time = jam_time[ind];
-  //    char temp_char = jam_char[ind];
-  //    for (int k = ind; k > 0; k--) {
-  //      jam_val[k] = jam_val[k - 1];
-  //      jam_char[k] = jam_char[k - 1];
-  //      jam_time[k] = jam_time[k - 1];
-  //    }
-  //    jam_val[0] = temp_val;
-  //    jam_char[0] = temp_char;
-  //    jam_time[0] = temp_time;
-  //  }
+//  int ind = find(current_route);
+//  if (ind != -1 && ind != 0) {
+//    int temp_val = jam_val[ind];
+//    int temp_time = jam_time[ind];
+//    char temp_char = jam_char[ind];
+//    for (int k = ind; k > 0; k--) {
+//      jam_val[k] = jam_val[k - 1];
+//      jam_char[k] = jam_char[k - 1];
+//      jam_time[k] = jam_time[k - 1];
+//    }
+//    jam_val[0] = temp_val;
+//    jam_char[0] = temp_char;
+//    jam_time[0] = temp_time;
+//  }
   // add the current route to the beigning of the array
   // then add the maximum value
   // if 2 elements or more are equal then sort by time
 }
-
-
 void display() {
-  /*
-  ** display the counter
-  ** if maximum value in jam array not equal current route
-  ** then set counter = 5 if the counter > 5
-  ** if maximum value in jam array == current route and counter < 3
-  ** then add 5 to counter
-  ** if counter == 0 then
-  ** set current_route == next_route [always = 1]
-  ** change_route to the current_route
-  ** set counter == 99
-  */
-  
+  // display the counter
+  // if maximum value in jam array not equal current route
+  // then set counter = 5 if the counter > 5
+  // if maximum value in jam array == current route and counter < 3
+  // then add 5 to counter
+  // if counter == 0 then
+  // set current_route == next_route [always = 1]
+  // change_route to the current_route
+  // set counter == 99
   for (int h = 1; h < 10000; h++) {
     sevseg.setNumber(counter);
     sevseg.refreshDisplay();
   }
-  
-  /*
-  ** if maximum value in jam array not equal current route
-  ** then set counter = 5 if the counter > 5
-  */
-  
-  current_route_val = jam_val[find('current_route')];
-  
-  if (current_route != max_jam && counter > 15 && max_jam_val != current_route_val) {
-    counter = 14;
-    
-  } else if ((current_route == max_jam && counter < 3) || (max_jam_val == current_route_val && counter < 3)) {
-    /*
-    ** if maximum value in jam array == current route and counter < 3
-    ** then add 5 to counter
-    */
-  
+  // if maximum value in jam array not equal current route
+  // then set counter = 5 if the counter > 5
+  if (current_route != max_jam && counter > 5) {
+    counter = 4;
+  }
+  // if maximum value in jam array == current route and counter < 3
+  // then add 5 to counter
+  else if (current_route == max_jam && counter < 3) {
     counter = counter + 5;
   } else {
-    /*
-    ** if counter == 0 then
-    ** set current_route == next_route [always = 1]
-    ** change_route to the current_route
-    ** set counter == 99
-    */
-    
+    // if counter == 0 then
+    // set current_route == next_route [always = 1]
+    // change_route to the current_route
+    // set counter == 99
     if (counter <= 0) {
       
       current_route = jam_char[1];
@@ -316,10 +274,14 @@ void display() {
       change_route(current_route);
     }
     counter--;
+    Serial.print(current_route);
+    Serial.print(" - ");
+    Serial.print(max_jam);
+    Serial.print(" - ");
+    Serial.print(counter);
+      Serial.print('\n');
   }
 }
-
-
 int find(char ele) {
   for (int i = 0; i < SIZE; i++) {
     if (jam_char[i] == ele) {
@@ -328,8 +290,6 @@ int find(char ele) {
   }
   return -1;
 }
-
-
 void change_route(char route) {
   switch (route) {
   case 'a':
